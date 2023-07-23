481e98b9cf2318636efdb4c04248c7eaf62a0c9a
sarthak9793 <sar.sri67@gmail.com>
2023-07-23 15:42:40 +0530

    change: position change

--- src/Board.jsx
+++ src/Board.jsx
@@ -23,7 +23,7 @@ export default function Board(){
         setCurrPlayer(1)
     }
     return(
-        <div className="mt-7">
+        <div className="mt-4">
             <div className="mx-auto text-4xl text-center mb-28 bg-custom-text-bg-color w-64 p-2 rounded-md">
                 Move: Player {currPlayer===1 ? "1" : "2"}
             </div>
